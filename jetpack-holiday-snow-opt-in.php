79966e70b96b7042722fb603097a6fd33f3bb23d
Josh Eaton <josh@josheaton.org>
2016-12-04 08:51:24 -0500

    Standards cleanup

--- jetpack-holiday-snow-opt-in.php
+++ jetpack-holiday-snow-opt-in.php
@@ -50,23 +50,23 @@ class Jetpack_Holiday_Snow_OptIn {
 	 */
 	private function __construct() {
 
-		// Get plugin path
+		// Get plugin path.
 		$this->plugin_path = dirname( plugin_dir_path( __FILE__ ) );
 
-		// Initialize snow status
+		// Initialize snow status.
 		$this->snowing = false;
 		$this->jetpack = false;
 
-		add_action( 'plugins_loaded', array( $this, 'jetpack_active_check'   ), 15 );
+		add_action( 'plugins_loaded', array( $this, 'jetpack_active_check' ), 15 );
 
-		// Load plugin text domain
-		add_action( 'init',           array( $this, 'load_plugin_textdomain' )     );
+		// Load plugin text domain.
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
 
-		// Load the plugin if we're in season
-		add_action( 'init',           array( $this, 'run_if_in_season'       ), 1  );
+		// Load the plugin if we're in season.
+		add_action( 'init', array( $this, 'run_if_in_season' ), 1 );
 
-		// Check for snow cookies (yum!) or snow control requests
-		add_action( 'init',           array( $this, 'process_snow_opt_in'    ), 2  );
+		// Check for snow cookies (yum!) or snow control requests.
+		add_action( 'init', array( $this, 'process_snow_opt_in' ), 2 );
 	}
 
 	/**
@@ -79,7 +79,7 @@ class Jetpack_Holiday_Snow_OptIn {
 	public static function get_instance() {
 
 		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
+		if ( null === self::$instance ) {
 			self::$instance = new self;
 		}
 
@@ -97,49 +97,44 @@ class Jetpack_Holiday_Snow_OptIn {
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
 		load_textdomain( $domain, WP_LANG_DIR . '/' . $domain . '/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+		load_plugin_textdomain( $domain, false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
 	}
 
 	public function jetpack_active_check() {
-
 		$this->jetpack = false;
 
-		// Confirm Jetpack is loaded and the holiday-snow.php file is loaded
+		// Confirm Jetpack is loaded and the holiday-snow.php file is loaded.
 		if ( class_exists( 'Jetpack' ) && function_exists( 'jetpack_is_holiday_snow_season' ) ) {
 			$this->jetpack = true;
 			return $this->jetpack;
 		}
 
 		return $this->jetpack;
-
 	}
 
 	public function run_if_in_season() {
-
 		if ( ! $this->jetpack ) {
 			return;
 		}
 
-		if ( ! jetpack_is_holiday_snow_season() )
+		if ( ! jetpack_is_holiday_snow_season() ) {
 			return;
+		}
 
-		// Add a filter so we can control the snow
+		// Add a filter so we can control the snow.
 		add_filter( 'jetpack_holiday_chance_of_snow', array( $this, 'should_it_snow' ) );
 
-		// Add our snow control and styles
-		add_action( 'wp_head',   array( $this, 'add_snow_css'    ) );
+		// Add our snow control and styles.
+		add_action( 'wp_head', array( $this, 'add_snow_css' ) );
 		add_action( 'wp_footer', array( $this, 'add_snow_opt_in' ) );
 	}
 
 	public function should_it_snow( $snow ) {
-
 		return $this->snowing;
-
 	}
 
 	public function add_snow_css() {
-
-		// check if the snow option is enabled, if not, bail
+		// Check if the snow option is enabled, if not, bail.
 		if ( ! get_option( jetpack_holiday_snow_option_name() ) ) {
 			return;
 		}
@@ -175,8 +170,7 @@ body.admin-bar #jetpack-holiday-snow-opt-in {
 	}
 
 	public function add_snow_opt_in() {
-
-		// check if the snow option is enabled, if not, bail
+		// Check if the snow option is enabled, if not, bail.
 		if ( ! get_option( jetpack_holiday_snow_option_name() ) ) {
 			return;
 		}
@@ -191,12 +185,13 @@ body.admin-bar #jetpack-holiday-snow-opt-in {
 		}
 
 		echo '<div id="jetpack-holiday-snow-opt-in">';
-			echo '<a href="' . add_query_arg( 'show_snow', $show_snow ) .  '" title="' . esc_attr( $title ) . '"><span>&#xFF0A;</span></a>';
+			echo '<a href="' . esc_url( add_query_arg( 'show_snow', $show_snow ) ) .  '" title="' . esc_attr( $title ) . '"><span>&#xFF0A;</span></a>';
 		echo '</div>';
 	}
 
 	public function has_snow_cookie() {
-		if ( isset( $_COOKIE[ 'show_me_the_snow' ] ) && '1' == $_COOKIE[ 'show_me_the_snow' ] ) {
+		if ( isset( $_COOKIE['show_me_the_snow'] ) // Input var okay.
+			&& 1 === absint( $_COOKIE['show_me_the_snow'] ) ) {
 			return true;
 		}
 
@@ -204,44 +199,41 @@ body.admin-bar #jetpack-holiday-snow-opt-in {
 	}
 
 	public function process_snow_opt_in() {
-
 		if ( ! $this->jetpack ) {
 			return;
 		}
 
-		// check if the snow option is enabled, if not, bail
+		// Check if the snow option is enabled, if not, bail.
 		if ( ! get_option( jetpack_holiday_snow_option_name() ) || ! jetpack_is_holiday_snow_season() ) {
 			return;
 		}
 
-		// If we want snow and haven't asked it to change
-		if ( $this->has_snow_cookie() && ! isset($_GET['show_snow']) ) {
+		// If we want snow and haven't asked it to change.
+		if ( $this->has_snow_cookie() && ! isset( $_GET['show_snow'] ) ) { // Input var okay.
 			$this->snowing = true;
 			return;
 		}
 
-		// If we haven't yet asked for snow, and haven't asked it to change
-		if ( !$this->has_snow_cookie() && ! isset($_GET['show_snow']) ) {
+		// If we haven't yet asked for snow, and haven't asked it to change.
+		if ( ! $this->has_snow_cookie() && ! isset( $_GET['show_snow'] ) ) { // Input var okay.
 			$this->snowing = false;
 			return;
 		}
 
 		// Only continue if we're changing snow states.
-
-		$show_snow =  $_GET['show_snow'];
+		$show_snow = absint( $_GET['show_snow'] ); // Input var okay.
 
 		// If user wants snow, set the cookie saying so
-		// If not, remove the cookie
-		if ( $show_snow == '1' ) {
-			setcookie( 'show_me_the_snow', '1', time()+3600*24*30, COOKIEPATH, COOKIE_DOMAIN, false);
+		// If not, remove the cookie.
+		if ( 1 === $show_snow ) {
+			setcookie( 'show_me_the_snow', '1', time() + 3600 * 24 * 30, COOKIEPATH, COOKIE_DOMAIN, false );
 			$this->snowing = true;
 		} else {
-			setcookie( 'show_me_the_snow', '1', time()-3600*24*30, COOKIEPATH, COOKIE_DOMAIN, false);
+			setcookie( 'show_me_the_snow', '1', time() - 3600 * 24 * 30, COOKIEPATH, COOKIE_DOMAIN, false );
 			$this->snowing = false;
 		}
 	}
-
 } // end class Jetpack_Holiday_Snow_OptIn
 
-// Get the class instance
+// Get the class instance.
 add_action( 'plugins_loaded', array( 'Jetpack_Holiday_Snow_OptIn', 'get_instance' ) );
